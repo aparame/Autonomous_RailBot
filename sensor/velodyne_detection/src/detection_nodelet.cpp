d17c7a14cff0340b9e614605475c25bbf7bdccd3
Weirong Yuan <yuan161@purdue.edu>
2021-10-22 02:47:42 -0400

    d

--- sensor/velodyne_detection/src/detection_nodelet.cpp
+++ sensor/velodyne_detection/src/detection_nodelet.cpp
@@ -4,19 +4,6 @@
 #include "velodyne_detection/detection.h"
 
 
-int main(int argc, char **argv) {
-  // ROS init
-  ros::init(argc, argv, "velodyne_detection_node");
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_priv("~");
-
-  // create Detection class
-  velodyne_detection::Detection detection(nh, nh_priv);
-
-  ros::spin();
-  return 0;
-}
-
 namespace velodyne_detection
 {
   class DetectionNodelet: public nodelet::Nodelet
