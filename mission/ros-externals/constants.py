390ba7f2a379127900ca1a2493ffd074fad371fe
Weirong Yuan <yuan161@purdue.edu>
2021-11-16 15:34:16 -0500

    mission

--- mission/ros-externals/constants.py
+++ mission/ros-externals/constants.py
@@ -5,5 +5,8 @@ LOGSTART = 2
 LOGEND = 3
 
 # Project-wide directories
-MISSION_HOME = "/home/mithuna/missions/"
+MISSION_HOME = "/home/mithuna/RailBot/ros-externals"
 
+
+# LIDAR THRESHOLD
+OBSTACLE_THRESHOLD = 3.3
