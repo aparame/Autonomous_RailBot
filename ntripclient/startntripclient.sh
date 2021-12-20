f14214a67e07742c75cfdc3c4f5d83be3021ab9e
AshwinNagarajan <einsteinashwin@gmail.com>
2021-12-20 18:29:45 -0500

    Update startntripclient.sh

--- ntripclient/startntripclient.sh
+++ ntripclient/startntripclient.sh
@@ -8,13 +8,15 @@ User='PUrobot2'
 Password='PUrobot2'
 Port='10000'
 Server='108.59.49.226'
-Serial=''
+Serial='/dev/ttyUSB0'
+Stream='RTCM3_MAX'
+mode=3
 
 DateStart=`date -u '+%s'`
 SleepMin=10     # Wait min sec for next reconnect try
 SleepMax=10000  # Wait max sec for next reconnect try
 (while true; do
-  ./ntripclient -s $Server -r $Port -u $User -p $Password -D $Serial
+  ./ntripclient -s $Server -r $Port -u $User -p $Password -D $Serial -B 38400 -m $Stream -M $mode
   if test $? -eq 0; then DateStart=`date -u '+%s'`; fi
   DateCurrent=`date -u '+%s'`
   SleepTime=`echo $DateStart $DateCurrent | awk '{printf("%d",($2-$1)*0.02)}'`
