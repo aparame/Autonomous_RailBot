6a180629c0ae1f160f13d2f53bb55c6b577abdc6
Weirong Yuan <yuan161@purdue.edu>
2021-12-21 17:34:34 -0500

    ntrip

--- ntripclient/startntripclient.sh
+++ ntripclient/startntripclient.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 #
 # $Id$
 # Purpose: Start ntripclient
@@ -10,13 +10,14 @@ Port='10000'
 Server='108.59.49.226'
 Serial='/dev/ttyUSB0'
 Stream='RTCM3_MAX'
-mode=3
+latlon='D:40.43:-86.91'
+mode=4
 
 DateStart=`date -u '+%s'`
-SleepMin=10     # Wait min sec for next reconnect try
-SleepMax=10000  # Wait max sec for next reconnect try
+SleepMin=2     # Wait min sec for next reconnect try
+SleepMax=5  # Wait max sec for next reconnect try
 (while true; do
-  ./ntripclient -s $Server -r $Port -u $User -p $Password -D $Serial -B 38400 -m $Stream -M $mode
+  ./ntripclient -s $Server -r $Port -u $User -p $Password -D $Serial -B 9600 -m $Stream -M $mode -L $latlon -T 1 -A 8
   if test $? -eq 0; then DateStart=`date -u '+%s'`; fi
   DateCurrent=`date -u '+%s'`
   SleepTime=`echo $DateStart $DateCurrent | awk '{printf("%d",($2-$1)*0.02)}'`
